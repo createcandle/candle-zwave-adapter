c52747c82280e145c082a47cc619308b05b871b2
flatsiedatsie <unfold@gmail.com>
2026-03-02 18:40:33 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -53,41 +53,34 @@ function build_cross_compiled() {
   docker run --rm -t -v $PWD:/build webthingsio/toolchain-${ADDON_ARCH}-${LANGUAGE_NAME}-${LANGUAGE_VERSION} bash -c "cd /build; ADDON_ARCH=${ADDON_ARCH} ./package.sh"
 }
 
-if [[ $LANGUAGE_VERSION -gt 14 ]]
-then
-  echo "The node version is greater than 14."
-  install_linux_cross_compiler
+
+case "${ADDON_ARCH}" in
+darwin-x64)
+  install_osx_compiler
   build_native
+  ;;
+
+linux-arm)
+  install_linux_cross_compiler
+  build_cross_compiled
+  ;;
+
+linux-arm64)
+  install_linux_cross_compiler
+  build_cross_compiled
+  ;;
+
+linux-x64)
+  install_linux_cross_compiler
+  build_cross_compiled
+  ;;
+
+*)
+  echo "Unsupported architecture"
+  exit 1
+  ;;
+esac
   
-else
-  echo "The node version is 14 or less."
-  case "${ADDON_ARCH}" in
-  darwin-x64)
-    install_osx_compiler
-    build_native
-    ;;
 
-  linux-arm)
-    install_linux_cross_compiler
-    build_cross_compiled
-    ;;
-
-  linux-arm64)
-    install_linux_cross_compiler
-    build_cross_compiled
-    ;;
-
-  linux-x64)
-    install_linux_cross_compiler
-    build_cross_compiled
-    ;;
-
-  *)
-    echo "Unsupported architecture"
-    exit 1
-    ;;
-  esac
-  
-fi
 
 
